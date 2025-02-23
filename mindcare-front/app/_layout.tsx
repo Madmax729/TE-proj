8584a10e8b0330ba3a3c50d949c8801d9c470dcd
Mayank7110 <mayankwankhede258@gmail.com>
2025-02-23 15:28:53 +0530

    auth

--- mindcare-front/app/_layout.tsx
+++ mindcare-front/app/_layout.tsx
@@ -1,15 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
+
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 import "../global.css"
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { tokenCache } from '@/lib/auth'
+import * as SecureStore from 'expo-secure-store'
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+
+function RootLayoutNav() {
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
+}
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -30,12 +49,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+    <ClerkLoaded>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </ClerkLoaded>
+  </ClerkProvider>
   );
 }
---
+++ mindcare-front/constants/index.ts
@@ -0,0 +1,21 @@
+export const onboarding = [
+    {
+      id: 1,
+      title: "Get Started",
+      description:
+        "Discover the convenience of finding your perfect peace of mind",
+    //   image: images.onboarding2,
+    },
+    {
+      id: 2,
+      title: "Let's go!",
+      description:
+        "Connect with Us Right from your home Via live chat",
+    //   image: images.onboarding3,
+    },
+  ];
+  
+  export const data = {
+    onboarding,
+  };
+  
